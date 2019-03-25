73410f8e947974c1be0bcb4af11543ca94b0bc02
Claista Guibert <gclaista@gmail.com>
2019-03-25 17:54:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,8 +5,7 @@ function updatePlaylist(obj, key, value) {
   return obj
 }
 
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist;
   return playlist
 }
-removeFromPlaylist(playlist, Slowdive)
